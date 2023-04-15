a438d5f2369f4e5f50d62cae53f93bb0a090bc4a
LeszekJasionek <leszek.jasionek.bee@outlook.com>
2023-04-15 15:11:02 +0200

    Add project files.

---
+++ MajorityElement/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace MajorityElement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution sol = new Solution();
+            List<int> A = new List<int>() { 2, 1, 2 };
+            int majority = sol.majorityElement(A);
+            Console.WriteLine(majority);
+        }
+    }
+
+
+    public class Solution
+    {
+        public int majorityElement(List<int> A)
+        {
+            Dictionary<int, int> counts = new Dictionary<int, int>();
+            int n = A.Count;
+
+            // Zliczanie liczby wystąpień każdego elementu w tablicy
+            foreach (int num in A)
+            {
+                if (counts.ContainsKey(num))
+                {
+                    counts[num]++;
+                }
+                else
+                {
+                    counts[num] = 1;
+                }
+            }
+
+            // Wyszukiwanie elementu większościowego
+            foreach (KeyValuePair<int, int> kvp in counts)
+            {
+                if (kvp.Value > n / 2)
+                {
+                    return kvp.Key;
+                }
+            }
+
+            // Zwracanie -1, gdy nie ma elementu większościowego
+            return -1;
+        }
+    }
+
+}
