e8af15cb8a724d3417acbd5c08adac4f36831def
LeszekJasionek <leszek.jasionek.bee@outlook.com>
2023-04-16 18:07:35 +0200

    Stack

--- Sets/Program.cs
+++ Sets/Program.cs
@@ -6,13 +6,13 @@
         {
             // Declare a sorted set of integer type
 
-            SortedSet<int> sortedSet= new SortedSet<int>();
+            SortedSet<int> sortedSet = new SortedSet<int>();
 
             // Add all even numbers from 1 to 15
 
-            for(int i = 1; i <= 15; i++) 
+            for (int i = 1; i <= 15; i++)
             {
-                if(i % 2 != 0 ) sortedSet.Add(i);
+                if (i % 2 == 0) sortedSet.Add(i);
             }
 
             // Remove the largest element
@@ -22,26 +22,28 @@
             // Remove the 2nd smallest element
 
             if (sortedSet.Count > 1) sortedSet.Remove(sortedSet.ElementAt(1));
-            
+
 
 
             // Insert first 5 odd numbers
 
-            for(int i = 1; i <= 9; i+=2)
+            for (int i = 1; i <= 9; i += 2)
             {
                 sortedSet.Add(i);
             }
 
             // Print the elements of the set separated by a space
 
-            foreach(int singleElementOfSortedList in sortedSet)
+            foreach (int singleElementOfSortedList in sortedSet)
             {
-                Console.WriteLine(singleElementOfSortedList + " ");
+                Console.Write(singleElementOfSortedList + " ");
             }
+            Console.WriteLine();
 
             // Print the size of the set
 
             Console.WriteLine(sortedSet.Count);
+
         }
     }
 }
---
+++ Stack/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Net;
+
+namespace Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare a stack of integer type
+
+            Stack<int> stackOfIntegers = new Stack<int>();
+
+            // Push the first 5 even numbers in the stack
+
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    stackOfIntegers.Push(i);
+                }
+            }
+
+            // Push the first 5 odd numbers in the stack
+
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    stackOfIntegers.Push(i);
+                }
+            }
+
+
+            // Print the size of the stack
+
+            Console.WriteLine(stackOfIntegers.Count);
+
+            // Remove the top most element of the stack
+
+            stackOfIntegers.Pop();
+
+            // Print the elements of the stack from top to bottom
+            // separated by space
+
+            foreach (int elementOfStack in stackOfIntegers)
+            {
+                Console.Write(elementOfStack + " ");
+            }
+
+
+        }
+    }
+}
