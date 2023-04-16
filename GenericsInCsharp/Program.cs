47c705700d337ff354bd61a9c64e704fe895343f
LeszekJasionek <leszek.jasionek.bee@outlook.com>
2023-04-17 00:39:31 +0200

    GenericsInCsharp

---
+++ GenericsInCsharp/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace GenericsInCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Printer.printArray(new string[6]);
+        }
+    }
+
+    public static class Printer
+    {
+          public static void printArray<T>(T[] arr)
+        {
+            foreach(T t in arr)
+            {
+                Console.WriteLine(t);
+            }
+        }
+    }
+}
